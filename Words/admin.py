df8f98da765298705e3d51f6b416c726fd9759c8
Aleksey Martunov <alekseymartunov@icloud.com>
2022-12-03 01:56:35 +0300

    add admin

---
+++ Words/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Words
+
+
+@admin.register(Words)
+class WordsAdmin(admin.ModelAdmin):
+    list_display = ('name_rus', 'name_eng', 'errors', 'correct', 'created', 'user_ip')
+    readonly_fields = ('errors', 'correct', 'created',)
